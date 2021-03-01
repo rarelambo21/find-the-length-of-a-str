1825d4cbe60bbf135d0eb188b15ee511b808de07
rarelambo21 <35502394+rarelambo21@users.noreply.github.com>
2021-03-01 10:15:18 +0300

    Str length using Iterator

--- str length/str length.cpp
+++ str length/str length.cpp
@@ -2,10 +2,26 @@
 //
 
 #include <iostream>
+#include <string>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main(){
+    std::string str;
+    std::string::iterator it;
+
+    str = "Hello World";
+
+    int count = 0;
+
+    for (it = str.begin(); it != str.end(); it++)
+    {
+        std::cout << *it;
+        count++;
+    }
+
+    std::cout << "\n";
+    std::cout << count << std::endl;
+
+    return 0;
 }
 
 
